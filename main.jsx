6cdd48dbdf1e3e524d08fa11c741bcead37586d4
asdj420 <houseofrajatva@gmail.com>
2025-06-26 01:34:54 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import KarakhanaOrderPanel from "./KarakhanaOrderPanel";
-import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <KarakhanaOrderPanel />
-  </React.StrictMode>
-);
